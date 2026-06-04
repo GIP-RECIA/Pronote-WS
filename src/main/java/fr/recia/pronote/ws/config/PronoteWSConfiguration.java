7ffcd2ae53f49364d63df122c591aa7c1eaf2be4
Ewann ROUX <ewann.roux@recia.fr>
2026-06-04 11:35:53 +0200

    test: add KeyConfiguration for test
    default bean try to read file containing a public key, in test it is overloaded with a bean generating a random key at start

--- src/main/java/fr/recia/pronote/ws/config/PronoteWSConfiguration.java
+++ src/main/java/fr/recia/pronote/ws/config/PronoteWSConfiguration.java
@@ -43,6 +43,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
@@ -103,6 +104,7 @@ public class PronoteWSConfiguration {
 
     @Bean
     @Lazy
+    @Profile("!test")
     public PublicKey publicKey() {
         try {
             final File file = Paths.get(indexEducationProperties.getPublicKeyPath()).toFile();
---
+++ src/test/java/fr/recia/pronote/ws/config/TestKeysConfig.java
@@ -0,0 +1,28 @@
+package fr.recia.pronote.ws.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PublicKey;
+
+
+@TestConfiguration
+public class TestKeysConfig {
+
+    @Bean
+    @Primary
+    PublicKey publicKey() throws Exception {
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(2048);
+
+        keyPair = generator.generateKeyPair();
+
+        return keyPair.getPublic();
+    }
+
+    static KeyPair keyPair;
+
+}
